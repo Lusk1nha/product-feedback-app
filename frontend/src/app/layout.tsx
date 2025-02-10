c49fa78566784445c4b0d95dc1083e9a82a3dd6b
Lucas Pedro da Hora <lucaspedro517@gmail.com>
2025-02-10 15:54:11 -0300

    🐞Fix: Adjusting preload to vercel deploy.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -7,6 +7,7 @@ import Providers from "./providers";
 const jostFont = Jost({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
+  preload: false,
 });
 
 export const metadata: Metadata = {
